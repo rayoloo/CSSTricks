1a4ad1085d1e75b49385242fabb00292d85dbeba
rayoloo <raymond.lam122@gmail.com>
2022-01-13 17:27:47 -0500

    Update Navbar.js

--- realestate/src/components/Navbar.js
+++ realestate/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { FaRegTimesCircle } from 'react-icons/fa'
+import { BsFillHouseFill } from 'react-icons/bs'
 
 const Navbar = () => {
 	return <div></div>
