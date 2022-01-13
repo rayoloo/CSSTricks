9f0aec4ea674fbe0cf2a9567454c2d13b6b0deeb
rayoloo <raymond.lam122@gmail.com>
2022-01-13 17:21:19 -0500

    Initial commit

---
+++ realestate/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+	return (
+		<div>
+			<h1>Does this work</h1>
+		</div>
+	)
+}
+
+export default App
---
+++ realestate/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+
+const Navbar = () => {
+	return <div></div>
+}
+
+export default Navbar
